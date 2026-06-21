fd96aef019b0152af2b1721999e169e01a95a67a
MonkeyD74 <alcala059@gmail.com>
2026-06-20 22:16:24 -0700

    revert: quitar autenticación NextAuth

--- app/acceso-denegado/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function AccesoDenegado() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "20vh" }}>
-      <h1>Acceso denegado</h1>
-      <p>Tu email no está autorizado. Contacta al administrador.</p>
-    </div>
-  )
-}
